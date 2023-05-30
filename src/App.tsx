681f5509ea3d930d17d66229f994345bde3d1051
Thibaud <contact.maciuk@gmail.com>
2023-05-30 23:10:33 +0200

    night REinit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
-import logogg from './pepe_coin.png';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logogg} className="App-logo" alt="logo" />
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
-
-
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Pays from './pays/pays';
 
 const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 root.render(
-    <React.StrictMode>
-        <Pays/>
-        <App/>
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
